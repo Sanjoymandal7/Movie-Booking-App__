a119f8b2ea9ea791d2d95811c29394183861fece
Sanjoymandal7 <116301672+Sanjoymandal7@users.noreply.github.com>
2026-08-13 22:38:54 +0530

    [200~ndal MINGW64 /d/DinoMovie (main)

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const colors = require('colors')
+const dotenv = require('dotenv')
+const connectDB = require('./db')
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
+
+dotenv.config();
+connectDB();
+const app = express()
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+app.use("/api/v1/user",userRoutes);
+app.use("/api/v1/order",orderRoutes);
+
+app.get('/',(req,res)=>{
+    res.status(200).send({
+        'message':"Node server"
+    })
+})
+
+const PORT = process.env.PORT || 8027;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
